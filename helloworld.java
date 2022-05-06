7c731074984090f98d3d8bd8d983b61584fa0097
Sbn1993 <102149859+Sbn1993@users.noreply.github.com>
2022-05-07 01:23:01 +0530

    Update helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -1,6 +1,6 @@
 class Simple{  
     public static void main(String args[]){  
      System.out.println("Hey everyone from batch26 team");
-      System.out.println("Hello Batch batch26 WITH FOR STUDENTS RUNNING in front of supriya and ritu and added the CI configuration from assistance with kalyani shilpi abhishek as soon as code commits");
+      System.out.println(" Making changes into the code : Hello Batch batch26 WITH FOR STUDENTS RUNNING in front of supriya and ritu and added the CI configuration from assistance with kalyani shilpi abhishek as soon as code commits");
     }  
 } 

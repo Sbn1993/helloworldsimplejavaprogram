47923dfc428d282a632acba6a68b5b79d02d8156
RashmiGarji <106993110+RashmiGarji@users.noreply.github.com>
2022-07-27 22:18:05 +0530

    Update helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -1,6 +1,7 @@
 class Simple{  
     public static void main(String args[]){  
      System.out.println("Hey everyone from AWS+DevOps Learning team");
+      System.out.println("Hey everyone from AWS+DevOps Learning team");
       System.out.println(" Making changes into the code : Hello  STUDENTS RUNNING in front of Sachin and Yallappa and added the CI configuration from assistance with Rashmi as soon as code commits");
     }  
 } 

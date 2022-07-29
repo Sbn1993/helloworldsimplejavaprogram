d383e3e2c5938a29a9d599f404f02cc9e66450ed
Sbn1993 <102149859+Sbn1993@users.noreply.github.com>
2022-07-29 17:36:09 +0530

    Update helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -1,7 +1,6 @@
 class Simple{  
     public static void main(String args[]){  
      System.out.println("Hey everyone from AWS+DevOps Learning team");
-      System.out.println("Hey everyone from AWS+DevOps Learning team");
-      System.out.println(" Making changes into the code : Hello  STUDENTS RUNNING in front of Sachin and Yallappa and added the CI configuration from assistance with Rashmi as soon as code commits");
+      System.out.println(" Making changes into the code : Hello Sachin and Yallappa added the CI configuration from assistance with Rashmi as soon as code commits");
     }  
 } 
